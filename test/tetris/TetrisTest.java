65920cecccd6178ffacf78e69c8a7b79637a1cc2
VarozXYZ <alvaropiro@gmail.com>
2025-03-14 11:22:30 +0100

    added more tests

--- test/tetris/TetrisTest.java
+++ test/tetris/TetrisTest.java
@@ -19,5 +19,20 @@ class TetrisTest {
 	void testInitialScoreIsZero() {
 		assertEquals(0, game.currentScore(), "The score doesn't initialize as zero");
 	}
-	
+	@Test
+	void testPieceCanMoveDown() {
+		Tetromino piece = new Tetromino(new Point[] {new Point(4,0), new Point(5,0)}, Color.RED);
+		assertTrue(board.canMove(piece, 0, 1), "The piece should be able to move down");
+	}
+	@Test
+	void testPieceCannotMoveOutsideLeft() {
+		Tetromino piece = new Tetromino(new Point[] {new Point(0,0), new Point(1,0)}, Color.BLUE);
+		assertFalse(board.canMove(piece, -1, 0), "The piece shouldn't be able to move outside the limits (left)");
+	}
+	@Test
+	void testLineClearIncreaseScore() {
+		for (int x = 0; x < 10; x++) {
+			board.grid[19][x] = Color.GREEN;
+		}
+	}
 }
